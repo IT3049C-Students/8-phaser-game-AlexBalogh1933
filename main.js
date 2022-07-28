1f5ad89141eaf2ffb63ab5be3c83163e3f217752
Alex <baloghaj@mail.uc.edu>
2022-07-28 17:50:05 -0400

    Added Coin

--- main.js
+++ main.js
@@ -35,42 +35,60 @@ function preload() {
 function create() {
     // load the map 
     map = this.make.tilemap({key: 'map'});
-    
     // tiles for the ground layer
     var groundTiles = map.addTilesetImage('tiles');
     // create the ground layer
     groundLayer = map.createDynamicLayer('World', groundTiles, 0, 0);
     // the player will collide with this layer
     groundLayer.setCollisionByExclusion([-1]);
+    // coin image used as tileset
+    var coinTiles = map.addTilesetImage('coin');
+    // add coins as tiles
+    coinLayer = map.createDynamicLayer('Coins', coinTiles, 0, 0);
     // set the boundaries of our game world
     this.physics.world.bounds.width = groundLayer.width;
     this.physics.world.bounds.height = groundLayer.height;
-
     // create the player sprite    
-    player = this.physics.add.sprite(200, 200, 'player'); 
+    player = this.physics.add.sprite(200, 200, 'player');
     player.setBounce(0.2); // our player will bounce from items
-    player.setCollideWorldBounds(true); // don't go out of the map
-
+    player.setCollideWorldBounds(true); // don't go out of the map    
+    
+    // small fix to our player images, we resize the physics body object slightly
+    player.body.setSize(player.width, player.height-8);
+    
+    // player will collide with the level tiles 
     this.physics.add.collider(groundLayer, player);
-
+    coinLayer.setTileIndexCallback(17, collectCoin, this);
+    // when the player overlaps with a tile with index 17, collectCoin 
+    // will be called    
+    this.physics.add.overlap(player, coinLayer);
+    // player walk animation
+    this.anims.create({
+        key: 'walk',
+        frames: this.anims.generateFrameNames('player', {prefix: 'p1_walk', start: 1, end: 11, zeroPad: 2}),
+        frameRate: 10,
+        repeat: -1
+    });
+    // idle with only one frame, so repeat is not neaded
+    this.anims.create({
+        key: 'idle',
+        frames: [{key: 'player', frame: 'p1_stand'}],
+        frameRate: 10,
+    });
     cursors = this.input.keyboard.createCursorKeys();
-
     // set bounds so the camera won't go outside the game world
     this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
     // make the camera follow the player
     this.cameras.main.startFollow(player);
-
     // set background color, so the sky is not black    
     this.cameras.main.setBackgroundColor('#ccccff');
-
-    this.anims.create({
-        key: 'walk',
-        frames: this.anims.generateFrameNames('player', { prefix: 'p1_walk', start: 1, end: 11, zeroPad: 2 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
 }
+
+function collectCoin(sprite, tile) {
+    coinLayer.removeTileAt(tile.x, tile.y); // remove the tile/coin
+    return false;
+}
+
 function update(time, delta) {    
     if (cursors.left.isDown)
     {
